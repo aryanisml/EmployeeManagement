8354475d741cf0be279cd98cfefd2f0456c866ee
swapnil <swapnil.kamble02@gmail.com>
2021-02-27 01:22:24 -0800

    routing

--- src/app/employee/employee-routing.module.ts
+++ src/app/employee/employee-routing.module.ts
@@ -2,15 +2,29 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EmployeeDetailsComponent } from './employee-details/employee-details.component';
 import { EmployeeCreationComponent } from './employee-creation/employee-creation.component';
+import { EmployeeComponent } from './employee.component';
 
 
-const employeeRoutes: Routes = [{
-    path: '/details',
-    component : EmployeeDetailsComponent
-}, {
-    path: '',
-    component: EmployeeCreationComponent
-}];
+const employeeRoutes: Routes = [
+{  
+    path :'',
+    component : EmployeeComponent,
+    children : [
+            {
+                path: '/details',
+                component : EmployeeDetailsComponent
+            }, {
+                path: '',
+                component: EmployeeCreationComponent
+            },
+        
+    ]
+
+}
+
+
+
+];
 
 @NgModule({
     imports: [RouterModule.forChild(employeeRoutes)],
