682a97e62dde2bb1f2bc6d116a7f061df80863fd
swapnil <swapnil.kamble02@gmail.com>
2021-02-27 02:57:38 -0800

    commit

--- src/app/employee/employee-details/employee-details.component.ts
+++ src/app/employee/employee-details/employee-details.component.ts
@@ -1,31 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { EmployeeServiceService } from '../employee-service.service';
-import {Employee} from '../employee';
+import { Employee } from '../employee';
 import { Observable } from 'rxjs';
 @Component({
   selector: 'app-employee-details',
   templateUrl: './employee-details.component.html',
-  styleUrls: ['./employee-details.component.scss']
+  styleUrls: ['./employee-details.component.scss'],
 })
 export class EmployeeDetailsComponent implements OnInit {
-
-  constructor(private _empService:EmployeeServiceService) { }
-  rowData$: Observable<Employee[]> | undefined;
+  constructor(private _empService: EmployeeServiceService) {
+    this.getEmployees();
+  }
+  // rowData$: Observable<Employee[]> | undefined;
+  rowData: any;
   ngOnInit(): void {
+    
   }
 
   columnDefs = [
-  
-    { empId: 'empId', sortable: true, filter: true },
+    { employeeId: 'empId', sortable: true, filter: true },
     { name: 'name', sortable: true, filter: true },
     { age: 'age', sortable: true, filter: true },
-    { phone: 'phone', sortable: true, filter: true },
+    { mobileNo: 'phone', sortable: true, filter: true },
     { email: 'email', sortable: true, filter: true },
-    { address: 'address', sortable: true, filter: true }
+    { address: 'address', sortable: true, filter: true },
   ];
-  getEmployees () {
-    this.rowData$ = this._empService.getEmployees()
-    console.log('EMPDATA:', this.rowData$);
+  getEmployees() {
+    // this.rowData$ = this._empService.getEmployees()
+    // console.log('EMPDATA:', this.rowData$);
+    this._empService.getEmployees().subscribe((d) => (this.rowData = d));
   }
-
 }
