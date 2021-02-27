6faab55564cb606d505495debafc04d153adebee
swapnil <swapnil.kamble02@gmail.com>
2021-02-26 22:43:21 -0800

    first commit

---
+++ src/app/employee/employee-creation/employee-creation.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { EmployeeCreationComponent } from './employee-creation.component';
+
+describe('EmployeeCreationComponent', () => {
+  let component: EmployeeCreationComponent;
+  let fixture: ComponentFixture<EmployeeCreationComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ EmployeeCreationComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(EmployeeCreationComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/employee/employee-creation/employee-creation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-employee-creation',
+  templateUrl: './employee-creation.component.html',
+  styleUrls: ['./employee-creation.component.scss']
+})
+export class EmployeeCreationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
