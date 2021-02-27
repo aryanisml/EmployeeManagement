54ac30bcba2aba509a3dfc8810c57a3d77e2e359
swapnil <swapnil.kamble02@gmail.com>
2021-02-27 01:52:52 -0800

    added routing

--- src/app/employee/employee.module.ts
+++ src/app/employee/employee.module.ts
@@ -4,6 +4,7 @@ import { EmployeeComponent } from '../employee/employee.component';
 import { EmployeeDetailsComponent } from './employee-details/employee-details.component';
 import { EmployeeCreationComponent } from './employee-creation/employee-creation.component';
 import { EmployeeRoutingModule } from './employee-routing.module';
+import { ShareModule } from '../share/share.module';
 
 
 
@@ -11,7 +12,8 @@ import { EmployeeRoutingModule } from './employee-routing.module';
   declarations: [EmployeeComponent, EmployeeDetailsComponent , EmployeeCreationComponent],
   imports: [
     CommonModule,
-    EmployeeRoutingModule
+    EmployeeRoutingModule,
+    ShareModule
   ]
 })
 export class EmployeeModule { }
