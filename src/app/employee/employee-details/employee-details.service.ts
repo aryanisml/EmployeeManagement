73650f94b4918f500ff163833c7e043ad4517108
suresh <sureshkathroj@gmail.com>
2021-02-28 17:06:35 +0530

    emp list updated

--- src/app/employee/employee-details/employee-details.service.ts
+++ src/app/employee/employee-details/employee-details.service.ts
@@ -8,10 +8,10 @@ export class EmployeeDetailsService {
 
   getColDef() {
     return [
-      { field: 'employeeId', sortable: true, filter: true },
+      { field: 'empId', sortable: true, filter: true },
       { field: 'name', sortable: true, filter: true },
       { field: 'age', sortable: true, filter: true },
-      { field: 'mobileNo', sortable: true, filter: true },
+      { field: 'phone', sortable: true, filter: true },
       { field: 'address', sortable: true, filter: true },
       {
         field: 'delete',
